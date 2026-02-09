6ba89b0d2cd2380dff560e23a6ba92a25249f102
Insha khan <inshakhan0503@gmail.com>
2026-02-10 01:21:30 +0530

    Create LC_21_MergeTwoSortedLists.java

---
+++ LC_21_MergeTwoSortedLists.java
@@ -0,0 +1,27 @@
+// Problem: Merge Two Sorted Lists
+// LeetCode No: 21
+// Approach: Iterative (Dummy Node)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;
+    }
+}
