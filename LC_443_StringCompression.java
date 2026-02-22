88cc2e41e90292d2383f487cd44c9898f6a391bf
Insha khan <inshakhan0503@gmail.com>
2026-02-22 15:06:33 +0530

    Create LC_443_StringCompression.java

---
+++ LC_443_StringCompression.java
@@ -0,0 +1,33 @@
+// Problem: String Compression
+// LeetCode No: 443
+// Approach: Two Pointer
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int compress(char[] chars) {
+        int index = 0;
+        int i = 0;
+
+        while (i < chars.length) {
+            char currentChar = chars[i];
+            int count = 0;
+
+            while (i < chars.length && chars[i] == currentChar) {
+                i++;
+                count++;
+            }
+
+            chars[index++] = currentChar;
+
+            if (count > 1) {
+                String countStr = String.valueOf(count);
+                for (char c : countStr.toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index;
+    }
+}
