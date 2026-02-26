f646255f4daec741cec37a8ec302ef23eadf73a7
Insha khan <inshakhan0503@gmail.com>
2026-02-26 23:45:52 +0530

    Create LC_334_IncreasingTripletSubsequence.java

---
+++ LC_334_IncreasingTripletSubsequence.java
@@ -0,0 +1,24 @@
+// Problem: Increasing Triplet Subsequence
+// LeetCode No: 334
+// Approach: Greedy (Track two minimums)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
