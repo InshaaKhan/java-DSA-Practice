a319ae189bd347a5bb6910a20ce5759427b5ab97
Insha khan <inshakhan0503@gmail.com>
2026-03-16 18:24:08 +0530

    Create LC_55_JumpGame.java

---
+++ LC_55_JumpGame.java
@@ -0,0 +1,20 @@
+// Problem: Jump Game
+// LeetCode No: 55
+// Approach: Greedy
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+}
